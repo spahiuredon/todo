bffb0c7235a35848b92088a838c4eff69e9682fb
spahiuredon <redon579@gmail.com>
2021-07-15 16:27:27 +0200

    tested CRUD of User with Postman

--- src/main/java/ch/bbw/rs/todo/controller/UserController.java
+++ src/main/java/ch/bbw/rs/todo/controller/UserController.java
@@ -3,6 +3,7 @@ package ch.bbw.rs.todo.controller;
 import ch.bbw.rs.todo.entity.Task;
 import ch.bbw.rs.todo.entity.User;
 import ch.bbw.rs.todo.repository.ApplicationUserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +22,37 @@ public class UserController {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+
+    //register a user
     @PostMapping("/sign-up")
     public void signUp(@RequestBody User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         applicationUserRepository.save(user);
     }
 
+
+    //get all users in a list
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<User> getUsers() {
         return applicationUserRepository.findAll();
     }
 
+
+    //find one user by the id
+    /*
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public User findUser(@PathVariable long id) {
+        User existingUser = applicationUserRepository.findById(id).get();
+        Assert.notNull(existingUser, "User not found");
+        return existingUser;
+    }
+*/
+
+    //change username of a user by id
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void editUser(@RequestBody User user, @PathVariable long id) {
         User existingUser = applicationUserRepository.findById(id).get();
         Assert.notNull(existingUser, "User not found");
@@ -40,6 +60,8 @@ public class UserController {
         applicationUserRepository.save(existingUser);
     }
 
+
+    //delete a user by id
     @DeleteMapping("{id}")
     public void deleteUser(@PathVariable long id) {
         User userToDel = applicationUserRepository.findById(id).get();
