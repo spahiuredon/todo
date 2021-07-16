d593052e73f8cb299f13614ef58ed0248a52bafd
spahiuredon <redon579@gmail.com>
2021-07-16 09:00:56 +0200

    added RoleRepository

---
+++ src/main/java/ch/bbw/rs/todo/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package ch.bbw.rs.todo.repository;
+
+import ch.bbw.rs.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
